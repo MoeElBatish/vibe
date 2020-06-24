36c4dfef24dbabd6fd41359d0b5e0e0b21f3fca9
MoeElBatish <moebatish@gmail.com>
2020-06-24 11:05:42 -0400

    finished play command

--- index.js
+++ index.js
@@ -2,9 +2,59 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+const ytdl = require("ytdl-core");
+
+var servers = {};
+
 client.once('ready', () => {
-    console.log('Welcome to Vibe!');
+    console.log('Vibe is online!');
 });
 
+client.on('message', message => {
+  var args = message.content.substring(PREFIX.length).split(" ");
 
-client.login('bot_id');
+  switch (args[0]) {
+    case 'play':
+
+      function play(connection,message){
+        var server = servers[message.guild.id]
+
+        server.dispatcher = connection.playStream(ytdl(server.queue[0], {filter: "audioonly"}));
+
+        server.queue.shift();
+
+        server.dispatcher.on("end", function() {
+          if(server.queue[0]) {
+            play(connection,message);
+          } else {
+            connection.disconnect();
+          }
+        });
+      }
+
+      if (!args[1]){
+        message.channel.send("provide a link please!");
+        return;
+      }
+
+      if(!message.member.voiceChannel) {
+        message.channel.send("You must be in a channel to play a song");
+        return;
+      }
+
+      if(!servers[message.guild.id]) servers[message.guild.id] = {
+        queue : []
+      }
+
+      var server = servers[message.guild.id];
+
+      server.queue.push(args[1]);
+
+      if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+        play(connection,message);
+      })
+  }
+})
+
+
+client.login('722699368917893141');
